599f489d5d6e6e4bbc88ca74f9465cd576067de6
amielc <amielc1@gmail.com>
2020-10-26 14:54:44 +0200

    Add Buy&Sell to demonstrate use coucurrent collection

---
+++ BuySell/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ConcurrentCollections.BuyAndSell
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StockController controller = new StockController();
+			TimeSpan workDay = new TimeSpan(0, 0, 0, 0, 500);
+
+            controller.DisplayStock();
+
+            Task t1 = Task.Run(() => new SalesPerson("Amiel").Work(workDay, controller));
+            Task t2 = Task.Run(() => new SalesPerson("Talya").Work(workDay, controller));
+            Task t3 = Task.Run(() => new SalesPerson("David").Work(workDay, controller));
+
+            Task.WaitAll(t1, t2, t3);
+
+            controller.DisplayStock();
+
+            Console.ReadKey();
+		}
+	}
+}
---
+++ BuySell/StockController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Collections.ObjectModel;
+
+namespace Pluralsight.ConcurrentCollections.BuyAndSell
+{
+	public class StockController
+	{
+		private Dictionary<string, int> _stock = new Dictionary<string, int>();
+		int _totalQuantityBought;
+		int _totalQuantitySold;
+		public void BuyShirts(string code, int quantityToBuy)
+		{
+			if (!_stock.ContainsKey(code))
+				_stock.Add(code, 0);
+			_stock[code] += quantityToBuy;
+			_totalQuantityBought += quantityToBuy;
+		}
+
+		public bool TrySellShirt(string code)
+		{
+			if (_stock.TryGetValue(code, out int stock) && stock > 0)
+			{
+				--_stock[code];
+				++_totalQuantitySold;
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public void DisplayStock()
+		{
+			Console.WriteLine("Stock levels by item:");
+			foreach (TShirt shirt in TShirtProvider.AllShirts)
+			{
+				_stock.TryGetValue(shirt.Code, out int stockLevel);
+				Console.WriteLine($"{shirt.Name,-30}: {stockLevel}");
+			}
+
+			int totalStock = _stock.Values.Sum();
+			Console.WriteLine($"\r\nBought = {_totalQuantityBought}");
+			Console.WriteLine($"Sold   = {_totalQuantitySold}");
+			Console.WriteLine($"Stock  = {totalStock}");
+			int error = totalStock + _totalQuantitySold - _totalQuantityBought;
+			if (error == 0)
+				Console.WriteLine("Stock levels match");
+			else
+				Console.WriteLine($"Error in stock level: {error}");
+		}
+	}
+}
